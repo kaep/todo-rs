4ce6c8669589cf92a2cc6f660456a1f9f1537db5
kaep <sebastianholmgaard@live.dk>
2026-04-02 10:19:40 +0200

    Add axum, required tokio features and API skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Router,
+    routing::{delete, get, post, put},
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/todos", get(get_todos))
+        .route("/todos", post(add_todo))
+        .route("/todos/{id}", put(complete_todo))
+        .route("/todos/{id}", delete(delete_todo));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_todos() -> String {
+    "Getting todos!".into()
+}
+
+async fn add_todo() -> String {
+    "Adding todo!".into()
+}
+
+async fn complete_todo() -> String {
+    "Completing todo!".into()
+}
+
+async fn delete_todo() -> String {
+    "Deleting todo!".into()
 }
